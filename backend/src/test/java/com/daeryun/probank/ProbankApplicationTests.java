c5170b19a5aa8de1cc9de0e9dff47ff9eb995548
Myong Hyo You <myonghyoyou@gmail.com>
2026-07-30 23:37:36 +0900

    chore: bootstrap Spring Boot backend project

---
+++ backend/src/test/java/com/daeryun/probank/ProbankApplicationTests.java
@@ -0,0 +1,12 @@
+package com.daeryun.probank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
